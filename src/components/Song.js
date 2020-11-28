7c43a819fca2d14ea457d35fe04da5219cda059a
parthpatel133 <prpatel133@gmail.com>
2020-11-28 10:57:51 +0530

    initial commit

---
+++ src/components/Song.js
@@ -0,0 +1,12 @@
+
+
+const Song = () => {
+    return (
+        <div className="song-container">
+            <h1>song</h1>
+            <h1>image</h1>
+        </div>
+    )
+}
+
+export default Song;
