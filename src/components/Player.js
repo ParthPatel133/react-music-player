70a5fdbba59c6ea8dc951b887ffd1dab7a38b796
parthpatel133 <prpatel133@gmail.com>
2020-11-29 10:06:55 +0530

    Player start and pause functionality

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,8 +1,23 @@
+import { useRef } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+    //Ref setting
+    const audioRef = useRef(null);
+
+    //Event Handeler wys
+    const playSongHandler = () => {
+        if (isPlaying) {
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying);
+        } else {
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+        }
+    }
+
     return (
         <div className='player'>
             <div className='time-control'>
@@ -12,9 +27,12 @@ const Player = () => {
             </div>
             <div className='play-control'>
                 <FontAwesomeIcon className='skip-back' size="2x" icon={faAngleLeft} />
-                <FontAwesomeIcon className='play' size="2x" icon={faPlay} />
+                <FontAwesomeIcon
+                    onClick={playSongHandler}
+                    className='play' size="2x" icon={faPlay} />
                 <FontAwesomeIcon className='skip-forward' size="2x" icon={faAngleRight} />
             </div>
+            <audio ref={audioRef} src={currentSong.audio} />
         </div>
     )
 }
