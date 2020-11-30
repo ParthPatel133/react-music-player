b566bb004e81a67ab7649a7e8ec2cfc8c6f1a2a7
parthpatel133 <prpatel133@gmail.com>
2020-11-30 11:48:16 +0530

    fix error

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,7 +3,7 @@
 const Song = ({ currentSong }) => {
     return (
         <div className="song-container">
-            <img src={currentSong.cover}></img>
+            <img src={currentSong.cover} alt={currentSong.name} ></img>
             <h2>{currentSong.artist}</h2>
             <h3>{currentSong.name}</h3>
         </div>
